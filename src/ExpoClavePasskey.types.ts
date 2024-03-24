ff34ba5be72457185984769952f5ae9ad83a6a54
zetsuboii <hmzkrb@gmail.com>
2024-03-24 05:55:15 +0300

    [expo-passkey] feat: add creation & authentication types

--- src/ExpoClavePasskey.types.ts
+++ src/ExpoClavePasskey.types.ts
@@ -1,3 +1,116 @@
+export type RelyingParty = {
+    /** Relying Party ID, can be any string */
+    id: string;
+    /** Relying Party name, can be any string */
+    name: string;
+};
+
+export type User = {
+    /** User ID, encoded as base64url */
+    id: string;
+    /** User name, can be any string */
+    name: string;
+    /** User display name, can be any string. This is what is shown to the user */
+    displayName: string;
+};
+
+export type PublicKeyCredentialParameters = {
+    /** @note Credential type is always 'public-key' */
+    type: string;
+    /**
+     * The algorithm used for the credential
+     * - `-7`: P256
+     * - `-257`: RS256
+     */
+    alg: number;
+};
+
+export type CredentialDescriptor = {
+    /** @note Credential type is always 'public-key' */
+    type: string;
+    /** ID for the credential, base64url encoded */
+    id: string;
+    /**
+     * The authenticator transports that the caller is willing to use.
+     * @note For mobile phone authenticators, use 'internal'
+     */
+    transports?: Array<
+        'usb' | 'ble' | 'nfc' | 'smart-card' | 'hybrid' | 'internal'
+    >;
+};
+
+/**
+ * Specifies requirements regarding authenticator attributes.
+ */
+export type AuthenticatorSelectionCriteria = {
+    /**
+     * "platform": Use platform specific authenticator.
+     * "cross-platform": Use cross-platform authenticator (e.g. security key)
+     * @note To use mobile phone as authenticator, set this to "platform"
+     */
+    authenticatorAttachment: 'platform' | 'cross-platform';
+    /**
+     * Whether the authenticator should support resident keys (aka passkeys).
+     * @note Default is "discouraged" if `requireResidentKey` is false and
+     * "required" else.
+     */
+    residentKey: 'discouraged' | 'preferred' | 'required';
+    /**
+     * Whether to use resident keys (aka passkeys).
+     */
+    requireResidentKey: boolean;
+    /**
+     * Specifies requirements regarding user verification.
+     * @note For mobile phone authenticators, this is ignored.
+     */
+    userVerification: 'preferred' | 'required' | 'discouraged';
+};
+
+export type PasskeyCreateOptions = {
+    /** Relying party options */
+    rp: RelyingParty;
+    /** User options */
+    user: User;
+    /** Challenge, base64url encoded */
+    challenge: string;
+    /** Public key credential params */
+    pubKeyCredParams: Array<PublicKeyCredentialParameters>;
+    /** Timeout for the operation, in milliseconds */
+    timeout?: number;
+    /** Credential IDs to excluded from creation */
+    excludeCredentials?: Array<CredentialDescriptor>;
+    /** Authenticator selection criteria */
+    authenticatorSelection?: AuthenticatorSelectionCriteria;
+    /** Information sent to relying party about the authenticator */
+    attestation?: 'none' | 'indirect' | 'direct' | 'enterprise';
+    /** Format of the attestation, can be ignored */
+    attestationFormats?: Array<string>;
+    /** Hints sent to authenticator, can be ignored */
+    hints?: Array<string>;
+    /** Extensions sent to authenticator, can be ignored */
+    extensions?: Record<string, unknown>;
+};
+
+export type PasskeyAuthenticationOptions = {
+    /** Challenge, base64url encoded */
+    challenge: string;
+    /** Timeout for the operation, in milliseconds */
+    timeout?: number;
+    /** Relying party ID */
+    rpId: RelyingParty['id'];
+    /** Allowed credentials for the operation */
+    allowCredentials?: Array<CredentialDescriptor>;
+    /**
+     * Specifies requirements regarding user verification.
+     * @note For mobile phone authenticators, this is ignored.
+     */
+    userVerification: 'preferred' | 'required' | 'discouraged';
+    /** Hints sent to authenticator, can be ignored */
+    hints?: Array<string>;
+    /** Extensions sent to authenticator, can be ignored */
+    extensions?: Record<string, unknown>;
+};
+
 /**
  * The available options for Passkey operations
  */
